de5f0e4a0e4e73e3befeb5a6d933e7852a0b480f
Edgar Araújo <edgararaj@gmail.com>
2024-03-08 17:55:23 +0000

    formatar codigo & adicionar clang-format gihub action

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  if (argc < 2)
-  {
-    printf("Uso: %s <output_folder> <parallel-tasks> <sched-policy>\n", argv[0]);
-    printf("Argumentos:\n");
-    printf("\toutput-folder: pasta onde são guardados os ficheiros com o output de tarefas executadas.\n");
-    printf("\tparallel-tasks: número de tarefas que podem ser executadas em paralelo.\n");
-    printf("\tsched-policy: identificador da política de escalonamento, caso o servidor suporte várias políticas.\n");
+    if (argc < 2) {
+        printf("Uso: %s <output_folder> <parallel-tasks> <sched-policy>\n", argv[0]);
+        printf("Argumentos:\n");
+        printf("\toutput-folder: pasta onde são guardados os ficheiros com o "
+               "output de tarefas executadas.\n");
+        printf("\tparallel-tasks: número de tarefas que podem ser executadas em "
+               "paralelo.\n");
+        printf("\tsched-policy: identificador da política de escalonamento, caso o "
+               "servidor suporte várias políticas.\n");
 
-    return 1;
-  }
-  printf("Hello, World!\n");
+        return 1;
+    }
+    printf("Hello, World!\n");
 }
